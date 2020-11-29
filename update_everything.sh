d0ff6aa6f007e06103e8e7219fa4cf6e16252665
gelakinetic <gelakinetic@gmail.com>
2020-11-29 11:57:54 -0500

    Automatic Updates

---
+++ update_everything.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Set up some variables
+DATE=$(date +"%Y-%m-%d")
+BRANCH_NAME=AUTO_$DATE
+
+# Reset the repo to main
+git checkout main
+git reset --hard
+git pull
+
+# Create and checkout a new branch
+git checkout -b $BRANCH_NAME
+
+# TODO run the updates
+git add --all
+
+# Commit and push the changes
+git commit -m "Automatic Updates"
+git push --set-upstream origin $BRANCH_NAME
+
+# Create the pull request
+gh auth login --with-token < gh-token.txt
+gh pr create --title "Automatic Updates $DATE" --body "See the diff"
