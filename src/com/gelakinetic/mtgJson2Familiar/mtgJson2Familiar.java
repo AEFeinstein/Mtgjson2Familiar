7a0bab4985c7fbe31362534b6936790b6c760ff2
gelakinetic <gelakinetic@gmail.com>
2020-11-29 23:13:12 -0500

    Remove git utils in favor of a bash script

--- src/com/gelakinetic/mtgJson2Familiar/mtgJson2Familiar.java
+++ src/com/gelakinetic/mtgJson2Familiar/mtgJson2Familiar.java
@@ -8,60 +8,38 @@ public class mtgJson2Familiar {
         boolean scrapeRules = false;
         boolean scrapeJudgeDocs = false;
         boolean buildPatches = false;
-        boolean nextArgIsUsername = false;
-        boolean nextArgIsPassword = false;
-        String gitUsername = null;
-        String gitPassword = null;
+        boolean printUsage = false;
+
         for (String arg : args) {
-            if (nextArgIsPassword) {
-                nextArgIsPassword = false;
-                gitPassword = arg;
-            } else if (nextArgIsUsername) {
-                nextArgIsUsername = false;
-                gitUsername = arg;
-            } else {
-                switch (arg) {
-                    case "-p": {
-                        buildPatches = true;
-                        break;
-                    }
-                    case "-r": {
-                        scrapeRules = true;
-                        break;
-                    }
-                    case "-j": {
-                        scrapeJudgeDocs = true;
-                        break;
-                    }
-                    case "-u": {
-                        nextArgIsUsername = true;
-                        break;
-                    }
-                    case "-w": {
-                        nextArgIsPassword = true;
-                        break;
-                    }
+            switch (arg) {
+                case "-p": {
+                    buildPatches = true;
+                    break;
+                }
+                case "-r": {
+                    scrapeRules = true;
+                    break;
+                }
+                case "-j": {
+                    scrapeJudgeDocs = true;
+                    break;
+                }
+                default: {
+                    printUsage = true;
+                    break;
                 }
             }
         }
 
-        if ((!scrapeRules && !scrapeJudgeDocs && !buildPatches) ||
-                ((gitUsername != null && gitPassword == null) || (gitUsername == null && gitPassword != null))) {
+        if (printUsage || (!scrapeRules && !scrapeJudgeDocs && !buildPatches)) {
             System.out.print("Usage\n" +
                     "  -p : build patches\n" +
                     "  -r : build comprehensive rules\n" +
-                    "  -j : build judge documents\n" +
-                    "  -u [username] : A git username to push changes\n" +
-                    "  -w [password] : A git password to push changes\n");
+                    "  -j : build judge documents\n");
             return;
         }
 
         int status = 0;
-
-        // If a git username and password were supplied, clean the repo first
-        if (gitUsername != null) {
-            GitUtils.gitCleanup();
-        }
 
         if (scrapeJudgeDocs) {
             if (!new JudgeDocScraper().ScrapeAll()) {
