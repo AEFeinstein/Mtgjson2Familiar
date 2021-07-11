51131c5b5cec17872a46ad6d5657e9fc02a52a02
gelakinetic <gelakinetic@gmail.com>
2021-07-10 21:51:08 -0400

    #37 Delete contents of downloads folder when done Better logging Comment and cleanup

--- src/com/gelakinetic/mtgJson2Familiar/JudgeDocConverter.java
+++ src/com/gelakinetic/mtgJson2Familiar/JudgeDocConverter.java
@@ -1,5 +1,6 @@
 package com.gelakinetic.mtgJson2Familiar;
 
+import org.jetbrains.annotations.Nullable;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
@@ -10,9 +11,11 @@ import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.util.Base64;
 import java.util.List;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@SuppressWarnings("BooleanMethodIsAlwaysInverted")
 public class JudgeDocConverter {
 
     /**
@@ -21,40 +24,51 @@ public class JudgeDocConverter {
     public static boolean GetLatestDocs() {
         boolean ret = true;
 
-        //noinspection RedundantIfStatement
-        if (null == processWpnDoc("https://wpn.wizards.com/en/document/magic-gathering-tournament-rules", Filenames.MTR_FILE)) {
+        m2fLogger.log(m2fLogger.LogLevel.INFO, "Processing judge docs");
+
+        if (!processWpnDoc("https://wpn.wizards.com/en/document/magic-gathering-tournament-rules", Filenames.MTR_FILE)) {
             ret = false;
         }
-        if (null == processWpnDoc("https://wpn.wizards.com/en/document/magic-gathering-judging-regular-rel", Filenames.JAR_FILE)) {
+        if (!processWpnDoc("https://wpn.wizards.com/en/document/magic-gathering-judging-regular-rel", Filenames.JAR_FILE)) {
             ret = false;
         }
-        if (null == processWpnDoc("https://wpn.wizards.com/en/document/magic-infraction-procedure-guide", Filenames.IPG_FILE)) {
+        if (!processWpnDoc("https://wpn.wizards.com/en/document/magic-infraction-procedure-guide", Filenames.IPG_FILE)) {
             ret = false;
         }
-        if (null == processJudgeAcademyDoc("https://judgeacademy.com/policy-documents/", Filenames.DIPG_FILE)) {
+        if (!processJudgeAcademyDoc(Filenames.DIPG_FILE)) {
             ret = false;
         }
-        if (null == processJudgeAcademyDoc("https://judgeacademy.com/policy-documents/", Filenames.DTR_FILE)) {
+        if (!processJudgeAcademyDoc(Filenames.DTR_FILE)) {
             ret = false;
         }
+
+        // Clean up downloads folder
+        for (File toDelete : Objects.requireNonNull(new File(Filenames.DOWNLOADS_DIR).listFiles())) {
+            if (!toDelete.delete()) {
+                m2fLogger.log(m2fLogger.LogLevel.ERROR, "Couldn't delete " + toDelete.getName());
+            }
+        }
+        m2fLogger.log(m2fLogger.LogLevel.INFO, "Done processing comprehensive rules");
 
         return ret;
     }
 
     /**
-     * TODO doc
+     * Download, convert, and process a document from Judge Academy
+     * After the document is processed, if it differs from the one in the rules folder, copy it to the rules folder
      *
-     * @param pageUrl
-     * @param outFile
-     * @return
+     * @param outFile The name of the file to write
+     * @return true if there were no errors, false if there were errors
      */
-    private static File processJudgeAcademyDoc(String pageUrl, String outFile) {
+    private static boolean processJudgeAcademyDoc(String outFile) {
+
+        m2fLogger.log(m2fLogger.LogLevel.INFO, "Processing Judge Academy, " + outFile);
 
         // Connect to the root page
-        Document docPage = NetUtils.ConnectWithRetries(pageUrl);
+        Document docPage = NetUtils.ConnectWithRetries("https://judgeacademy.com/policy-documents/");
         if (null == docPage) {
-            m2fLogger.log(m2fLogger.LogLevel.ERROR, "Couldn't connect to " + pageUrl);
-            return null;
+            m2fLogger.log(m2fLogger.LogLevel.ERROR, "Couldn't connect to https://judgeacademy.com/policy-documents/");
+            return false;
         }
 
         // Find all links in the page
@@ -74,27 +88,31 @@ public class JudgeDocConverter {
                     File dlFile = new File(downloadFolder, pdfFileName);
 
                     // Download and process the PDF
-                    downloadAndProcessPDF(linkStr, dlFile, outFile);
+                    return downloadAndProcessPDF(linkStr, dlFile, outFile);
                 }
             }
         }
-        return null;
+        m2fLogger.log(m2fLogger.LogLevel.ERROR, "Document not found (" + outFile + ")");
+        return false;
     }
 
     /**
-     * TODO doc
+     * Download, convert, and process a document from Wizards Play Network
+     * After the document is processed, if it differs from the one in the rules folder, copy it to the rules folder
      *
-     * @param pageUrl
-     * @param outFile
-     * @return
+     * @param pageUrl The URL for the page which has the PDF link in it
+     * @param outFile The name of the file to write
+     * @return true if there were no errors, false if there were errors
      */
-    private static File processWpnDoc(String pageUrl, String outFile) {
+    private static boolean processWpnDoc(String pageUrl, String outFile) {
+
+        m2fLogger.log(m2fLogger.LogLevel.INFO, "Processing Wizards Play Network, " + outFile);
 
         // Connect to the root page
         Document docPage = NetUtils.ConnectWithRetries(pageUrl);
         if (null == docPage) {
             m2fLogger.log(m2fLogger.LogLevel.ERROR, "Couldn't connect to " + pageUrl);
-            return null;
+            return false;
         }
 
         // Find all links in the page
@@ -111,57 +129,70 @@ public class JudgeDocConverter {
                 File dlFile = new File(downloadFolder, pdfFileName);
 
                 // Download and process the PDF
-                downloadAndProcessPDF(linkStr, dlFile, outFile);
+                return downloadAndProcessPDF(linkStr, dlFile, outFile);
             }
         }
-        return null;
+        m2fLogger.log(m2fLogger.LogLevel.ERROR, "Document not found (" + outFile + ")");
+        return false;
     }
 
     /**
-     * TODO
+     * Download, convert, and process a PDF document.
+     * It starts as a PDF, then is converted using pdftohtml to a webpage.
+     * The webpage has all external images embedded as base64 data and internal links fixed.
+     * After the document is processed, if it differs from the one in the rules folder, copy it to the rules folder
      *
-     * @param linkStr
-     * @param dlFile
-     * @param outFileName
+     * @param linkStr     A link to the PDF to download and process
+     * @param dlFile      The file to download the PDF to
+     * @param outFileName The name for the output processed HTML file
+     * @return true if there were no errors, false if there were errors
      */
-    private static void downloadAndProcessPDF(String linkStr, File dlFile, String outFileName) {
+    private static boolean downloadAndProcessPDF(String linkStr, File dlFile, String outFileName) {
         try {
             // Download the new file
             InputStream in = new URL(linkStr).openStream();
             Files.copy(in, dlFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
 
-            // Process the PDF
-            // Once downloaded, convert it
+            // Once downloaded, convert it to HTML
             File htmlFile = convertPDFtoHTML(dlFile);
             if (null == htmlFile) {
                 m2fLogger.log(m2fLogger.LogLevel.ERROR, "Couldn't convert " + dlFile.getName());
-                return;
+                return false;
             }
 
-            // Once converted, merge images into single file
+            // Once converted, merge images into single file and fix links
             File finalFile = embedBase64Images(htmlFile, outFileName);
             if (null == finalFile || !finalFile.exists()) {
+                assert finalFile != null;
                 m2fLogger.log(m2fLogger.LogLevel.ERROR, finalFile.getName() + " doesn't exist");
+                return false;
             }
 
             // Compare the new doc with the existing one, copy it over if necessary
-            compareJudgeDoc(outFileName);
+            return compareJudgeDoc(outFileName);
         } catch (IOException e) {
             m2fLogger.log(m2fLogger.LogLevel.ERROR, "Couldn't download " + linkStr);
+            return false;
         }
     }
 
     /**
-     * TODO doc
+     * Convert a PDF to HTML using pdftohtml.
+     * pdftohtml MUST be installed on the host system for this function to work
      *
-     * @param pdfFile
-     * @return
+     * @param pdfFile The PDF file to convert
+     * @return The resulting HTML file or null if there was a failure
      */
+    @Nullable
     private static File convertPDFtoHTML(File pdfFile) {
+
+        // Set up the system call to pdftohtml
         ProcessBuilder builder = new ProcessBuilder();
         builder.command("pdftohtml", "-s", "-c", pdfFile.getName());
         builder.directory(pdfFile.getParentFile());
+
         try {
+            // Call pdftohtml
             Process process = builder.start();
             if (0 == process.waitFor()) {
                 // This is how pdftohtml renames files
@@ -177,17 +208,18 @@ public class JudgeDocConverter {
                 return null;
             }
         } catch (IOException | InterruptedException e) {
-            m2fLogger.log(m2fLogger.LogLevel.ERROR, "Couldn't execute " + builder.toString());
+            m2fLogger.log(m2fLogger.LogLevel.ERROR, "Couldn't execute " + builder);
             return null;
         }
     }
 
     /**
-     * TODO doc
+     * Take the HTML output of pdftohtml, embed all external images as base64 data, and fix internal links
      *
-     * @param toEmbedIn
-     * @param outName
+     * @param toEmbedIn The HTML file to merge images into and fix links in
+     * @param outName   The processed HTML file, or null if there is an error
      */
+    @Nullable
     static File embedBase64Images(File toEmbedIn, String outName) {
         File directory = toEmbedIn.getParentFile();
 
@@ -197,6 +229,7 @@ public class JudgeDocConverter {
         Base64.Encoder encoder = Base64.getEncoder();
 
         File mergedFile = new File(directory, outName);
+
         try (BufferedReader br = new BufferedReader(new FileReader(toEmbedIn));
              BufferedWriter bw = new BufferedWriter(new FileWriter(mergedFile))) {
 
@@ -226,44 +259,13 @@ public class JudgeDocConverter {
     }
 
     /**
-     * TODO doc
+     * Compare a document in the downloads and rules folder.
+     * If they're different, copy it from downloads to rules
      *
-     * @param f
-     * @return
+     * @param outFileName The filename to compare
+     * @return true if the operation was successful, false if there was an error
      */
-    private static String getFileExtension(File f) {
-        String extension = "";
-
-        int i = f.getName().lastIndexOf('.');
-        if (i > 0) {
-            extension = f.getName().substring(i + 1);
-        }
-        return extension;
-    }
-
-    /**
-     * TODO
-     *
-     * @param f
-     * @param extraCharsToTrim
-     * @return
-     */
-    private static String getFilenameNoExtension(File f, int extraCharsToTrim) {
-        String noExtension = "";
-
-        int i = f.getName().lastIndexOf('.');
-        if (i > 0) {
-            noExtension = f.getName().substring(0, i - extraCharsToTrim);
-        }
-        return noExtension;
-    }
-
-    /**
-     * TODO doc
-     *
-     * @param outFileName
-     */
-    private static void compareJudgeDoc(String outFileName) {
+    private static boolean compareJudgeDoc(String outFileName) {
         boolean filesEqual = true;
 
         try {
@@ -271,18 +273,22 @@ public class JudgeDocConverter {
             List<String> dlLines = Files.readAllLines(new File(Filenames.DOWNLOADS_DIR, outFileName).toPath());
             List<String> ruLines = Files.readAllLines(new File(Filenames.RULES_DIR, outFileName).toPath());
 
-            // Eat the date line
-            ruLines.remove(0);
-
-            if (dlLines.size() != ruLines.size()) {
-                // Different line numbers, definitely not equal
+            if (ruLines.isEmpty()) {
                 filesEqual = false;
             } else {
-                for (int i = 0; i < dlLines.size(); i++) {
-                    if (!dlLines.get(i).equals(ruLines.get(i))) {
-                        // Lines not equal
-                        filesEqual = false;
-                        break;
+                // Eat the date line
+                ruLines.remove(0);
+
+                if (dlLines.size() != ruLines.size()) {
+                    // Different line numbers, definitely not equal
+                    filesEqual = false;
+                } else {
+                    for (int i = 0; i < dlLines.size(); i++) {
+                        if (!dlLines.get(i).equals(ruLines.get(i))) {
+                            // Lines not equal
+                            filesEqual = false;
+                            break;
+                        }
                     }
                 }
             }
@@ -303,12 +309,47 @@ public class JudgeDocConverter {
                 // Copy line by line
                 String line;
                 while (null != (line = br.readLine())) {
-                    bw.write(line);
+                    bw.write(line + '\n');
                 }
             } catch (IOException e) {
-                e.printStackTrace();
-                // TODO error
+                m2fLogger.log(m2fLogger.LogLevel.ERROR, "Couldn't embed base64 images " + e.getMessage());
+                return false;
             }
         }
+        // All done
+        return true;
+    }
+
+    /**
+     * Utility function to return a file extension
+     *
+     * @param f The file object to get an extension from
+     * @return The file extension in String form
+     */
+    private static String getFileExtension(File f) {
+        String extension = "";
+
+        int i = f.getName().lastIndexOf('.');
+        if (i > 0) {
+            extension = f.getName().substring(i + 1);
+        }
+        return extension;
+    }
+
+    /**
+     * Utility function to return a filename without the extension
+     *
+     * @param f                The file object to get a name from
+     * @param extraCharsToTrim The number of characters to trim before the extension
+     * @return The file extension in String form
+     */
+    private static String getFilenameNoExtension(File f, int extraCharsToTrim) {
+        String noExtension = "";
+
+        int i = f.getName().lastIndexOf('.');
+        if (i > 0) {
+            noExtension = f.getName().substring(0, i - extraCharsToTrim);
+        }
+        return noExtension;
     }
 }
